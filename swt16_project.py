335ae9315cecb2ab57abee29831e58443886b26e
lenatech <chichen@mail.uni-mannheim.de>
2016-11-14 09:07:36 +0100

    read input json format and get all recipes directly by using sparql, close #13, close #14, close #8

--- swt16_project.py
+++ swt16_project.py
@@ -42,14 +42,14 @@ class Socket(object):
 
 class Querier(object):
 
-    def sendQuery(self,query,dataset):
+    def sendQuery(self, query, dataset):
         sparql = SPARQLWrapper("http://localhost:3030/"+dataset+"/query")
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
         return results
 
-    def find_food_id(self,food_name):
+    def find_food_id(self, food_name):
         dataset = "F1"
         query = """
                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -62,13 +62,17 @@ class Querier(object):
         food_id = result["food_id"]["value"][-8:]
         return food_id
 
-    def find_recipe_id (self, food_id):
+    def find_recipe_ids(self, priority_food_ids, other_food_ids):
+        priorities = ", ".join(str(x) for x in self.combinePrefix("kasabif:", priority_food_ids))
+        others = " || ".join(str(x) for x in self.combinePrefix("?other = kasabif:", other_food_ids))
+
         dataset = "F2"
         query = """
                 PREFIX recipe: <http://linkedrecipes.org/schema/>
                 PREFIX kasabif: <http://data.kasabi.com/dataset/foodista/food/>
                 SELECT ?recipe_id WHERE{
-                    GRAPH ?graph { ?recipe_id recipe:ingredient kasabif:"""+food_id+""" .}
+                    GRAPH ?graph {?recipe_id recipe:ingredient """+priorities+""", ?other.}
+                    FILTER("""+others+""")
                 }
                 """
         results = self.sendQuery(query, dataset)
@@ -78,7 +82,11 @@ class Querier(object):
             recipe_of_food.append(result["recipe_id"]["value"][-8:])
         return recipe_of_food
 
-    def find_recipe_info (self, recipe_id):
+    def combinePrefix(self, prefix, id_list):
+        L = [prefix + food_id for food_id in id_list]
+        return L
+
+    def find_recipe_info(self, recipe_id):
         dataset = "F2"
         query = """
                 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -94,7 +102,6 @@ class Querier(object):
         recipe_link = result["recipe_link"]["value"]
         recipe_image = result["recipe_image"]["value"]
         return (recipe_link, recipe_image)
-
 
 class Parser(object):
     def __init__(self):
@@ -126,72 +133,49 @@ class Parser(object):
 
 if __name__ == '__main__':
     while True:
-        HOST = '172.20.10.2'
+        HOST = '192.168.0.10'
         PORT = 2222
 
         sk = Socket()
         conn = sk.create_conn(HOST, PORT)
         receive_string = sk.receive(conn)
-
-        food_name_list = []
-        for food in receive_string.split(","):
-            food_name_list.append(food)
-
-        print "Ingredients_Name: ", food_name_list
+        data_loaded = json.loads(receive_string)
 
         querier = Querier()
 
-        all_recipes = []
-        for food_name in food_name_list:
-            food_id = querier.find_food_id(food_name)
-            all_recipes.append(querier.find_recipe_id(food_id))
-
-        #Need to optimize this part
-        for x in range(len(all_recipes)):
-            if len(all_recipes)==1:
-                recipe_id=all_recipes[0]
-                break
+        priority_food_ids = []
+        other_food_ids = []
+        for k, v in data_loaded.items():
+            if v == 1:
+                food_id = querier.find_food_id(k)
+                priority_food_ids.append(food_id)
             else:
-                if (x+1)<len(all_recipes):
-                    r1 = all_recipes[0]
-                    r2 = all_recipes[x+1]
-                    all_recipes[0]= [val for val in r1 if val in r2]
-                    recipe_id = all_recipes[0]
-                else:
-                    break
+                food_id = querier.find_food_id(k)
+                other_food_ids.append(food_id)
+
+        recipe_id = querier.find_recipe_ids(priority_food_ids, other_food_ids)
 
         parser = Parser()
 
-        result_dict ={}
-        for m in recipe_id:
-            recipe_link, recipe_img = querier.find_recipe_info(m)
-            print "recipe_img: ", recipe_img
-            print "recipe_link: ", recipe_link
-
+        result_dict = []
+        for x in range(len(recipe_id)):
+            recipe = {}
+            recipe_link, recipe_img = querier.find_recipe_info(recipe_id[x])
             parser.parse(recipe_link)
-            title = parser.getTitle()[0]
-            print "Recipe_Name: ", title
-            print '\n'
 
-            print 'Brief_Ingredients:'
-            ingredients = parser.getIngredients()
-            for l in ingredients:
-                print l
-            print '\n'
+            recipe["recipeName"] = parser.getTitle()[x]
+            recipe["description"] = " "
+            recipe["imgURL"] = recipe_img
+            recipe["ingredients"] = parser.getIngredients()
+            recipe["preparation"] = parser.getPreparation()
+            recipe["tags"] = " "
+            recipe["techniques"] = " "
+            recipe["tools"] = " "
+            recipe["related"] = " "
 
-            print 'Steps_Detatils: '
-            preparation = parser.getPreparation()
-            for m in preparation:
-                print m
-            print '\n'
-
-            result_dict["Recipe_Name"]= parser.getTitle()
-            result_dict["Recipe_image"] = recipe_img
-            result_dict["Brief_Ingredients"]= parser.getIngredients()
-            result_dict["Steps_Detatils"] = parser.getPreparation()
+            result_dict.append(recipe)
 
         serialized_dict = json.dumps(result_dict)
-        #print "serialized_dict", serialized_dict
 
         sk.send(conn, serialized_dict)
         sk.close(conn)
