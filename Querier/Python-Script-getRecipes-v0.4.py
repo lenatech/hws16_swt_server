2b127b7b3ba5f579bc86c4324f044625a574fc4e
farboda <farbod.ameri@gmail.com>
2016-11-19 11:01:46 +0100

    Update Python-Script-getRecipes-v0.4.py

--- Querier/Python-Script-getRecipes-v0.4.py
+++ Querier/Python-Script-getRecipes-v0.4.py
@@ -1,8 +1,11 @@
 '''
 changes: 
-Using Enhanced SPARQL query
+adding tag frequency function
 '''
 import os
+from SPARQLWrapper import SPARQLWrapper, JSON
+import json
+
 
 def gettingTheIdFromTheLinkStringRecipe ( data ):
 	recipes=[]
@@ -36,6 +39,23 @@ def gettingTheIdFromTheLinkStringName ( data ):
 	
 	return nameID
 
+
+def gettingTheTagNames ( data ):
+	tags=[]
+	i=0
+	itemp=0
+	j=0
+	while i<len(data):
+		if data[i]=='a' and data[i+1]=='g' and data[i+2]=='s' and data[i+3]=='/':
+			itemp=i+4
+			tags.append("")
+			while data[itemp]!='>':
+				tags[j]+=data[itemp]
+				itemp+=1
+			j+=1
+		i += 1
+	return tags
+
 def sendQueryToFuseki ( oneIngredientName ):
 	queryString='rsparql --service http://localhost:3030/F2/query "'
 	queryString+="""PREFIX recipe: <http://linkedrecipes.org/schema/> PREFIX kasabif: <http://data.kasabi.com/dataset/foodista/food/> SELECT ?recipe_id WHERE{GRAPH ?graph { ?recipe_id recipe:ingredient kasabif:"""+oneIngredientName+""" .}}"""
@@ -66,6 +86,34 @@ def findingTheIdofTheName(arrayOfIngredients):
 		newArrayOfIngredients.append(gettingTheIdFromTheLinkStringName(data))
 	return newArrayOfIngredients
 
+def gettingTags(recId):
+	queryString='rsparql --service http://localhost:3030/F2/query "'
+	queryString+="""PREFIX recipe: <http://linkedrecipes.org/schema/> PREFIX recID: <http://data.kasabi.com/dataset/foodista/recipe/> SELECT ?recipe_id WHERE{GRAPH ?graph { recID:"""+recId+""" recipe:category ?recipe_id.}}"""
+	queryString+='"'
+	a=os.popen(queryString)
+	data=a.read()
+	a.close()
+	return gettingTheTagNames(data)
+
+def countTags(recId):
+	recString=""
+	for i in range(len(recId)):
+		recString+=" ?other = recID:"+str(recId[i])
+		if i != (len(recId)-1):
+			recString+=" ||"
+	queryString="""PREFIX recipe: <http://linkedrecipes.org/schema/> PREFIX recID: <http://data.kasabi.com/dataset/foodista/recipe/> SELECT ?tag (count(?tag) as ?count) (SAMPLE(?other) as ?myRecID) WHERE{{ GRAPH ?graph { ?other recipe:category ?tag.} FILTER( """+recString+""" )} union {GRAPH ?graph {?other recipe:category ?tag. }FILTER("""+recString+""")}} GROUP BY ?tag ORDER BY DESC(?count) LIMIT 5"""
+        sparql = SPARQLWrapper("http://localhost:3030/F2/query")
+        sparql.setQuery(queryString)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+	tagFrequency=[]
+	for i in range(len(results["results"]["bindings"])):
+		result = results["results"]["bindings"][i]
+		tagFrequency.append([])
+		tagFrequency[i].append(result["tag"]["value"][45:])
+		tagFrequency[i].append(result["myRecID"]["value"][47:])
+	return  tagFrequency
+
 def main():
 	temp = open ("ingredients.txt", "r+")
 	arrayOfIngredientsName = temp.read().splitlines()
@@ -91,10 +139,21 @@ def main():
 				finalId=allRecipesArray[0]
 			else:
 				break
+	
+	'''
+	tagArray=[]
+	aCounter=0
+	while aCounter<len(finalId):
+		tagArray.append(gettingTags(finalId[aCounter]))
+		aCounter+=1
+	print len(tagArray)
+	for i in range(len(tagArray)):
+		print tagArray[i]
+	'''
 	print '**************'
 	print 'Recipe ID: ',finalId
 	print '**************'
 	print 'Number of Recipes: ',len(finalId)
 	print '**************'
-
+	print countTags(finalId)
 main()
